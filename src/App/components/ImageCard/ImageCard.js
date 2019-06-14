68e7cfe6c842b35c87283727c89e2dbec96e148f
Alan Maldonado <business.alanm@gmail.com>
2019-06-13 21:30:55 -0500

    creating-refs

--- src/App/components/ImageCard/ImageCard.js
+++ src/App/components/ImageCard/ImageCard.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
   render() {
     const { description, urls } = this.props.image;
     return (
       <React.Fragment>
         <div>
-          <img alt={description} src={urls.regular} />
+          <img alt={description} src={urls.regular} ref={this.imageRef} />
         </div>
       </React.Fragment>
     );
