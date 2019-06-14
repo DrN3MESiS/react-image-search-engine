44d60de183b6b79975688df18334d527005161e0
Alan Maldonado <business.alanm@gmail.com>
2019-06-13 21:52:08 -0500

    final-commit

--- src/App/components/ImageCard/ImageCard.js
+++ src/App/components/ImageCard/ImageCard.js
@@ -7,11 +7,23 @@ class ImageCard extends React.Component {
     this.imageRef = React.createRef();
   }
 
+  state = { spans: null };
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const span = Math.ceil(height / 150);
+    this.setState({ spans: span });
+  };
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
   render() {
     const { description, urls } = this.props.image;
     return (
       <React.Fragment>
-        <div>
+        <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
           <img alt={description} src={urls.regular} ref={this.imageRef} />
         </div>
       </React.Fragment>
